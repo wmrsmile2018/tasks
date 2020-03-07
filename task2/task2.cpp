b26fea0b49313a6052c02985092fde4b9be393dd
wmrsmile2018 <wmrsmile2018@gmail.com>
2020-03-08 02:28:41 +0300

    task2

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -13,6 +13,7 @@ float isAtRight(Point p1, Point p2, Point p3) {
   a = (p1.y - p2.y);
   b = (p2.x - p1.x);
   c = (p1.x * p2.y - p2.x * p1.y);
+  // cout << a * p3.x + b * p3.y + c << endl;
   return a * p3.x + b * p3.y + c;
 }
 
@@ -29,21 +30,24 @@ float isInTriangle(Point edg1, Point edg2, Point edg3, Point p) {
   res2 = isAtRight(edg2, edg3, p);
   if (res1 < 0 && res2 < 0)
     return 2; // если под углом ?????
-  if (isEdge(edg1, p) || isEdge(edg2, p) || isEdge(edg3, p)) // если лежит на вершине
-    return 0;
-  if (res1 == 0 || res2 == 0 ) // если лежит на стороне
-    return 1;
+  if (isEdge(edg1, p) || isEdge(edg2, p) || isEdge(edg3, p))
+    return 0; // если лежит на вершине
+  if (res1 == 0 || res2 == 0 )
+    return 1; // если лежит на стороне
   return 3;
 }
 
 int isInRectangle(Point *rec, Point p) {
-  int flag1, flag2;
+  int flag1, flag2, flag3, flag4;
 
   flag1 = isInTriangle(rec[3], rec[0], rec[1], p);
   flag2 = isInTriangle(rec[1], rec[2], rec[3], p);
+  flag3 = isInTriangle(rec[2], rec[3], rec[0], p);
+  flag4 = isInTriangle(rec[0], rec[1], rec[2], p);
   if (flag1 == 0 || flag2 == 0)
     return 0; // вершина
-  if (flag1 == 1 || flag2 == 1)
+  if (((flag1 == 1 && flag2 == 2) || (flag2 == 1 && flag1 == 2))
+      && ((flag3 == 1 && flag4 == 2) || (flag4 == 1 && flag3 == 2)))
     return 1; // сторона
   if (flag1 == 2 && flag2 == 2)
     return 2; // внутри
@@ -53,14 +57,10 @@ int isInRectangle(Point *rec, Point p) {
 
 int main(int argc, char **argv) {
   Point rec[4], p;
-
   string line, s;
-
-  if (argc != 3)
-    return 1;
-
   fstream recP(argv[1]);
   fstream points(argv[2]);
+
   if (recP.is_open()) {
     for (int i = 0; getline(recP, line) && i < 4; i ++) {
         s = line.c_str();
